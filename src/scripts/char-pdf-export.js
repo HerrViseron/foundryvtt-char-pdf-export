87997fed1d5300197b2f5eecb1077ecf43f797e3
Daniel <admin@viseron.de>
2025-02-15 23:25:19 +0100

    Basic module functionality and preparation for the export feature

--- src/scripts/char-pdf-export.js
+++ src/scripts/char-pdf-export.js
@@ -1,30 +1,42 @@
-
-
 class CharPDFExport {
     static ID = 'char-pdf-export';
 
-    static TEMPLATES = {
-        CHARPDFEXPORT: `modules/${this.ID}/templates/char-pdf-export.hbs`
+    static SETTINGS = {
+        ENABLED: 'enabled',
+        PDF_FILE: 'pdf-file'
     }
 
     //A basic logging function
     static log(...args) {
-        console.log(this.ID, ' | ', ...args);
+        console.log(this.ID, '|', ...args);
+    }
+
+    static initialize() {
+        this.log("Initializing...")
+        
+        // Actual initialization:
+        game.settings.register(this.ID, this.SETTINGS.ENABLED, {
+            name: `CHAR-PDF-EXPORT.settings.${this.SETTINGS.ENABLED}.Name`,
+            hint: `CHAR-PDF-EXPORT.settings.${this.SETTINGS.ENABLED}.Hint`,
+            config: true,
+            scope: 'world',
+            type: Boolean,
+            default: true,
+            requiresReload: true,
+        });
+        game.settings.register(this.ID, this.SETTINGS.PDF_FILE, {
+            name: `CHAR-PDF-EXPORT.settings.${this.SETTINGS.PDF_FILE}.Name`,
+            hint: `CHAR-PDF-EXPORT.settings.${this.SETTINGS.PDF_FILE}.Hint`,
+            config: true,
+            scope: 'world',
+            type: String,
+            default: "",
+            filePicker: true,
+        });
+        // Finished with inizialization
+        this.log("Char PDF Exporter is initialized!");
     }
 }
-
-Hooks.on('renderActorSheet5eCharacter', (actorSheet5eCharacter2, html) => {
-    const charSheetHeaderCopyUUIDItem = html.find(`[data-tooltip="SHEETS.CopyUuid"]`)
-    
-    CharPDFExport.log("Hook executed");
-
-    const tooltip = game.i18n.localize('CHAR-PDF-EXPORT.PDFHeaderButton')
-    charSheetHeaderCopyUUIDItem.before(
-        <a class='char-pdf-export-header-button pseudo-header-button' data-tooltip='${tooltip}' data-tooltip-direction='DOWN'>
-            <i class='fa-solid fa-file-pdf'></i>
-        </a>
-    )
-});
 
 class CharPDFExportData {
     
@@ -35,3 +47,32 @@ class CharPDFExportData {
     static createCharPDFSheet(charData) {}
 
 }
+
+Hooks.on('renderActorSheet5eCharacter', (actorSheet5eCharacter2, html) => {
+    if (!game.settings.get(CharPDFExport.ID, CharPDFExport.SETTINGS.ENABLED)){
+        CharPDFExport.log("PDF Export is disabled in settings!")
+        return;
+    }
+    
+    const charSheetHeaderCopyUUIDItem = html.find(`[data-tooltip="SHEETS.CopyUuid"]`)
+
+    const tooltip = game.i18n.localize('CHAR-PDF-EXPORT.PDFHeaderButton')
+    charSheetHeaderCopyUUIDItem.before(`\
+        <a class='char-pdf-export-header-button pseudo-header-button' data-tooltip='${tooltip}' data-tooltip-direction='DOWN'>\
+            <i class='fa-solid fa-file-pdf'></i>\
+        </a>\
+    `)
+
+    html.on('click', '.char-pdf-export-header-button', (event) => {
+        if (game.settings.get(CharPDFExport.ID, CharPDFExport.SETTINGS.PDF_FILE) == ""){
+            CharPDFExport.log("ERROR: No PDF File set!")
+            return;
+        }
+        CharPDFExport.log("Generating Char PDF Sheet using", game.settings.get(CharPDFExport.ID, CharPDFExport.SETTINGS.PDF_FILE))
+    });
+
+});
+
+Hooks.once('init', () => {
+    CharPDFExport.initialize();
+});
