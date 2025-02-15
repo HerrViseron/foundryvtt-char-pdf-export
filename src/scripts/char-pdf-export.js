b5ba1dcbebda0218c6fef5350f41dc77da633686
Daniel <admin@viseron.de>
2025-02-15 19:02:01 +0100

    Basic Class declarations and added a Button to the Char Sheet Header

---
+++ src/scripts/char-pdf-export.js
@@ -0,0 +1,30 @@
+
+
+class CharPDFExport {
+    static ID = 'char-pdf-export';
+
+    static TEMPLATES = {
+        CHARPDFEXPORT: `modules/${this.ID}/templates/char-pdf-export.hbs`
+    }
+}
+
+Hooks.on('renderActorSheet5eCharacter', (ActorSheet5eCharacter2, html) => {
+    const charSheetHeaderCopyUUIDItem = html.find(`[data-tooltip="SHEETS.CopyUuid"]`)
+
+    const tooltip = game.i18n.localize('CHAR-PDF-EXPORT.PDFHeaderButton')
+    charSheetHeaderCopyUUIDItem.before(
+        <a class='char-pdf-export-header-button pseudo-header-button' data-tooltip='${tooltip}' data-tooltip-direction='DOWN'>
+            <i class='fa-solid fa-file-pdf'></i>
+        </a>
+    )
+});
+
+class CharPDFExportData {
+    
+    //get all Character Information / Data
+    static get characterData() {}
+
+    //create the PDF File
+    static createCharPDFSheet(charData) {}
+
+}
